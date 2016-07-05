4b6742d1e96d5cb6ef1a9ddd3be4f811ddf55de3
Артур Васильев <fotonartur@gmail.com>
2016-07-05 20:30:52 +0300

    Надо научиться обрабатывать исключения.

--- kks_console/alice_main.cpp
+++ kks_console/alice_main.cpp
@@ -7,6 +7,7 @@
 //---------------------------------------
 //Прототипы функций
 	#include "common.cpp"
+	void generation(AnB::AnBDriverAPI &board);
 
 //---------------------------------------
 //Точка входа
@@ -16,33 +17,42 @@ int main( void )
 	using namespace AnB;
 
 	//Инициализация платы
-
-	struct device_list boards;//Здесь хранятся указателя на все обнаруженные устройства
-	try{
-		if (!AnBDriverAPI::GetDevicesList(boards)) 
+	
+	//Здесь хранятся указатели на все обнаруженные устройства
+	struct device_list boards;
+	AnBDriverAPI *board;
+	while (true)
+	{
+		try{
+			if (!AnBDriverAPI::GetDevicesList(boards)) throw errno;
+			board = boards.array[0].dev_ref;
+			if (!board->SetDev(Alice)) 
+			{
+				cerr << board->LastError() << endl;
+				board->~AnBDriverAPI();
+				continue;
+			} else break;
+		} catch(int e)
 		{
 			cerr << boards.array[0].dev_ref->LastError() << endl;
+			cerr << strerror(e) << endl;
 		}
-	} catch(...)
-	{
-
-	}
-	AnBDriverAPI &board = *boards.array[0].dev_ref;
-	
+		
+	};
 
 	while ( true )	//Самый внешний цикл - заставляет повториться всю программу,
 					//если что-то пошло не так
 	try
 	{
-    char hostname[] = "localhost4";
-    char port[] = "50000";
-		NetWork bob( hostname, port );
+		char hostname[] = "localhost4";
+		char port[] = "50000";
+		//NetWork bob_sock( hostname, port );
 		//Теперь создадим цикл, в котором будем крутить основной алгоритм
 		//работы
 		while (true)//рабочий цикл
 		{
-			std::cout << "alice: Successfully connected with Bob" << std::endl;
-			
+			std::cout << "alice: Successfully connected with bob_sock" << std::endl;
+			generation(*board);//, bob_sock);
 			break;
 		}//end рабочий цикл
 
@@ -57,3 +67,45 @@ int main( void )
 
 //--------------------------------------------------
 //Определения функций Алисы
+
+//Алгоритм генерации ключа
+void generation(AnB::AnBDriverAPI &board)
+{
+	AnBRegInfo reg;
+	reg.address = RegTable;
+	board.RegRawRead(reg);
+	reg.value.table.size = 32;
+	reg.value.table.mode = Mode2;
+	board.RegRawWrite(reg);
+
+	//Сформируем таблицу RNG
+	uint8_t val[8];
+	for (uint8_t i = 0; i < 8; i++) val[i] = i;
+	board.WriteTable((char*)val, reg.value.table.size, TableRNG);
+
+	//std::cout << board.GetDump(BAR0); 
+	//board.DMADisable();
+	//std::cout << board.GetDump(BAR0);
+	//std::cout << "TableRNG" << std::endl;
+	//std::cout << board.GetDump(BAR1_RNG);
+	//board.DMADisable();
+	//std::cout << "DMAEnable = " << board.DMAEnable() << std::endl;
+	char *buf;
+	for (int i = 0; i < 16; i++) board.DMARead(buf);
+
+	while (true)
+	{
+		if (board.DMAIsReady()) board.DMARead(buf);
+		else 
+		{	
+			if (!board.DMADisable()) std::cerr << board.LastError() << std::endl;
+			std::cout << "DMA is not ready" << std::endl;
+			if (!board.DMAEnable()) std::cerr << board.LastError() << std::endl;
+		}
+	}	
+
+	for (int i = 0; i < AnBBufferMinSize; i++)
+	{
+		std::cout << *(buf+i);
+	} 
+};
--- kks_console/build_alice.sh
+++ kks_console/build_alice.sh
@@ -1,4 +1,10 @@
-g++ -g -std=c++11 -pthread -c -I./driverAnB/devel alice_main.cpp
+g++\
+    -g\
+    -std=c++11\
+    -pthread\
+    -c\
+    -I./driverAnB/devel\
+    alice_main.cpp
 ld -r alice_main.o ./driverAnB/devel/libAnB.a -o alice.o
 g++ -pthread alice.o -o alice.out
 rm alice_main.o
