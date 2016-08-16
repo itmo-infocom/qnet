4a72874ba01038e3f43cd91a0275c89a8c8bbd03
Артур Васильев <fotonartur@gmail.com>
2016-08-16 14:51:13 +0300

    реализовал server

--- kks_console/NetWork.h
+++ kks_console/NetWork.h
@@ -128,19 +128,19 @@ namespace send_recv
 				throw except("Recv vector<unsigned int>");
 		}
 
-		void send_recv::Send(detections &d, peers p)
+		void send_recv::Send(detections &d)
 		{
-			Send(d.basis, p);
-			Send(d.key, p);
-			Send(d.special, p);
-			Send(d.count, p);
+			Send(d.basis);
+			Send(d.key);
+			Send(d.special);
+			Send(d.count);
 		};
-		void send_recv::Recv(detections &d, peers p)
+		void send_recv::Recv(detections &d)
 		{
-			Recv(d.basis, p);
-			Recv(d.key, p);
-			Recv(d.special, p);
-			Recv(d.count, p);
+			Recv(d.basis);
+			Recv(d.key);
+			Recv(d.special);
+			Recv(d.count);
 		};
 	}
 
@@ -154,119 +154,29 @@ namespace send_recv
 		string hostname;
 		string port;
 
-		//Процедура выполняющая подключение к hostname через порт port
+		//Процедура выполняющая подключение к hostname к порту port
 		void connection_sequence(void);
 	};
 
-	static void *acception( void* arg );//Существует в отдельном потоке и принимает входящие подключения - создаётся только для сервера
 	class server: public details::send_recv
 	{
-		server(char *port);
-	};
+		//Конструктор. flag - для указания работы в неблокирующем (SOCK_NONBLOCK) режиме (чтобы Боб не зависал GUI, который может никогда и не подключиться)
+		server(char *port, int flag = 0);
 
-	class send_recv
-	{
-	public:
-		send_recv( int _max_cli, char *_port );//Сервер
-		send_recv( char *_hostname, char *_port );//Клиент
-		~send_recv();
-		
-		void Send( int cmd, peers p = peers_size )
-			{
-				void *pcmd = &cmd;
-				PrivateSend(p, pcmd, sizeof(cmd));
-			};
-		void Send( void *cmd, size_t size, peers p = peers_size ) 
-			{PrivateSend(p, cmd, size);};
-		void Recv( int &cmd, int p = peers_size ) 
-			{
-				void *pcmd = &cmd;
-				PrivateRecv(p, pcmd, sizeof(cmd));
-			};
-		void Recv( void *&cmd, size_t size, peers p = peers_size )
-			{PrivateRecv(p, cmd, size);};
-		
-		void Send(std::vector<bool> &v, peers p = peers_size);
-		void Recv(std::vector<bool> &v, peers p = peers_size);
-
-		void Send(std::vector<unsigned int> &v, peers p = peers_size);
-		void Recv(std::vector<unsigned int> &v, peers p = peers_size);
-
-		void Send(detections &d, peers p = peers_size);
-		void Recv(detections &d, peers p = peers_size);
-		
-		bool IsReady(int p);//Возвращает - подключён ли клиент p
-		
-		//Список команд, которые передаём по сети
-		enum NetComds
-		{
-		};
+		//Принимает входящее соединение с флагом flag - в блокирующем или неблокирующем режиме.
+		//Возвращает успешность принятия соединения. 
+		bool accept_cli(void);
 	private:
-		int fd;//Дескриптор сокета
-		int *clients;//Дескрипторы клиентов
-		int *cli_types;//Типы присоединившихся клиентов
-		int clients_num;//Текущее число подключенных клиентов
-		char *hostname;//Адрес хоста для подключения
-		int max_cli;//Максимальное число подключенных клиентов
-		char *port;//Номер порта или имя сервиса (напр.ftp, http)  в виде текстовой строки (напр."50000" иди "google.com")
-		bool server;//Определяет тип сокета - сервер или клиент
-		
-		pthread_t thread_acception;//поток, в котором слушается сокет на входящие соединения
-		pthread_mutex_t mtx_cli_list = PTHREAD_MUTEX_INITIALIZER;//Блокировка списка дескрипторов клиентов
-		pthread_mutex_t mtx_server = PTHREAD_MUTEX_INITIALIZER;//Блокировка на соединение с сервером
-		friend void* acception( void* arg );
-		
-		void PrivateSend( int p, void *cmd, size_t size ) throw(except)
-		{
-			//TODO: Сделать так, чтобы при разрыве после восстановления соединения всё продолжналось с того же места
-			int fd;
-			if (p < peers_size) fd = clients[p];
-			while (true)
-			if (send(fd, cmd, size, MSG_WAITALL) == -1) throw except("send_recv send");
-			else break;
-		};
-
-		void PrivateRecv( int p, void *&cmd, size_t size )
-		{
-			int fd;
-			cmd = new unsigned char [size];
-			if (p < peers_size) fd = clients[p];
-			while (true)
-				if (recv( fd, cmd, size, MSG_WAITALL ) != -1) break;
-					else {
-						if (server) sleep(1);
-						else send_recv(hostname, port);
-					}
-		};
+		//Дескриптор сокета - для клиента используется fd из details::send_recv
+		int socket_fd;
 	};
 
-	//Поток, который устанавливает входящие подключения
-	static void* acception( void* arg )
-	{
-		send_recv *obj = (send_recv*)arg;
-			while (true)
-			{
-				int client = accept( obj->fd, NULL, NULL );
-				int *peer_type;
-				//После установки соединения клиент представляется
-				recv(client, peer_type, sizeof(int), MSG_WAITALL);
-				
-				pthread_mutex_lock(&obj->mtx_cli_list);
-				if ( *peer_type < peers_size ) obj->clients[*peer_type] = client;
-					else close(client);
-				pthread_mutex_unlock(&obj->mtx_cli_list);
-			}
-		return 0;
-	}
-
-	send_recv::send_recv( int _max_cli, char* _port )
+	server::server(char* _port, int flag)
 	{ 
-		max_cli = _max_cli;
-		server = true;
 		struct addrinfo hints;
 		hints.ai_family = AF_UNSPEC;
 		hints.ai_socktype = SOCK_STREAM;
-		hints.ai_flags |= AI_PASSIVE;
+		hints.ai_flags |= AI_PASSIVE + flag;
 		hints.ai_protocol = 0;
 		hints.ai_canonname = NULL;
 		hints.ai_addr = NULL;
@@ -274,35 +184,41 @@ namespace send_recv
 		
 		struct addrinfo *res = nullptr;
 		
-		if ( getaddrinfo( NULL, _port, &hints, &res ) != 0 ) 
-		throw except("getaddrinfo");
-		
-		struct addrinfo *rp;//указатель на один из вариантов, к кому подключаться
-		for ( rp = res; rp != NULL; rp = rp->ai_next ) {
-			fd = socket( rp->ai_family, rp->ai_socktype, rp->ai_protocol );
-			if ( fd == -1 ) 
-				continue;
+		//Кусок ниже - это копия из мануала. Вся эта канитель только ради использования getaddrinfo
+		{
+			if ( getaddrinfo( NULL, _port, &hints, &res ) != 0 ) 
+			throw except("getaddrinfo");
+			
+			struct addrinfo *rp;//указатель на один из вариантов, к кому подключаться
+			for ( rp = res; rp != NULL; rp = rp->ai_next ) {
+				socket_fd = socket( rp->ai_family, rp->ai_socktype, rp->ai_protocol );
+				if ( fd == -1 ) 
+					continue;
 
-			if ( bind( fd, rp->ai_addr, rp->ai_addrlen ) == 0 )
-				break;                  /* Success */
+				if ( bind( socket_fd, rp->ai_addr, rp->ai_addrlen ) == 0 )
+					break;                  /* Success */
 
-			close(fd);
+				close(socket_fd);
+			}
+			
+			if ( rp == NULL ) //Ни один из адресов не подошёл
+				throw except("bind");
+			
+			freeaddrinfo(res);
 		}
-		
-		if ( rp == NULL ) //Ни один из адресов не подошёл
-			throw except("bind");
-		
-		freeaddrinfo(res);
-		listen(fd, _max_cli);
-		
-		clients_num = 0;
-		clients = new int[clients_num];
-		cli_types = new int[clients_num];
-		for (int i = 0; i < clients_num; i++) clients[i] = -1;
-		
-		pthread_create( &thread_acception, NULL, &acception, &fd );
+
+		listen(socket_fd, 1);
 	};
 
+	bool server::accept_cli(void)
+	{
+		if ((fd = accept(socket_fd, NULL, NULL)) == -1)
+		{
+			if (errno == EWOULDBLOCK || errno == EAGAIN) return false;
+			else throw except("accept");
+		} else return true;
+	};
+	
 	client::client(char *_hostname, char *_port)
 	{
 		fd = -1;
@@ -350,27 +266,5 @@ namespace send_recv
 
 		freeaddrinfo(res);
 	}
-
-	send_recv::~send_recv()
-	{
-		pthread_cancel(thread_acception);
-		pthread_mutex_destroy(&mtx_cli_list);
-		delete clients;
-		delete cli_types;
-	};
-
-	
-
-	bool send_recv::IsReady(int p)
-	{
-		pthread_mutex_lock(&mtx_cli_list);
-			if (p < peers::peers_size) return clients[p] == -1;
-			else 
-		{
-		fd = -1;
-		return fd;
-		}
-		pthread_mutex_unlock(&mtx_cli_list);
-	};
 }
 #endif
