f4b8a0efce1c5df524d42d0fb606acea5a8641ef
Arthur Vasilev <avasilev@localhost.localdomain>
2016-08-26 15:05:51 +0300

    Теперь в начале строки отправляется число бит ключа

--- tests/KeyByCURL/main.cpp
+++ tests/KeyByCURL/main.cpp
@@ -8,7 +8,7 @@
 
 const std::string URL = "http://localhost4/qchannel/1/1";
 
-int main (void)
+int main (int argc, char** argv)
 {
     using namespace std;
 
@@ -24,10 +24,14 @@ int main (void)
     //Если чисто бит не кратно четырём, то ненужные биты с конца отбрасываем -
     //невелика потеря
     {
-        string cmd = "curl --data-ascii ";
+        string cmd = "curl --data-ascii \"";
         stringstream ss;
         //Отбросим некратные четырём биты с конца
         key.resize((key.size()/4)*4);
+
+        //Добавим число байт
+        ss << key.size() << '\n';
+
         //Теперь будем формировать по одному hex-символу и добавлять ко строке
         for (size_t i = 0; i < key.size()/4; i++)
         {
@@ -35,8 +39,16 @@ int main (void)
             for (size_t j = 0; j < 4; j++) val |= key[i*4 + j] << j;
             ss << setbase(16) << val;
         }
+        ss << "\" http://";
+
+        //Если URL назначения не указан, то по умолчанию это localhost4
+        if (argc == 1) ss << "localhost4";
+        else ss << argv[1];
+
+        //Добавим статус канала
+        ss << "/qchannel/1/1";
+
         cmd += ss.str();
-        cmd += ' ' + URL;
         system(cmd.c_str());
         //cout << cmd << endl;
     }
