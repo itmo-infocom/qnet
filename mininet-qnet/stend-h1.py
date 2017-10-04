dcf9242c014816b796691c8220dfa0b2a55e5243
Kompaniets Mikhail <mkompan@gmail.com>
2017-10-04 13:39:54 +0300

    stend configs

---
+++ mininet-qnet/stend-h1.py
@@ -0,0 +1,6 @@
+config = { 'hostn': 1,
+           'hosts': [1,2],
+           'intf' : 'ens33',
+           'controller_ip' : '192.168.244.243',
+           'controller_port': 6633
+         }
---
+++ mininet-qnet/stend-h2.py
@@ -0,0 +1,6 @@
+config = { 'hostn': 2,
+           'hosts': [3,4],
+           'intf' : 'ens34',
+           'controller_ip' : '192.168.244.243',
+           'controller_port': 6633
+         }
