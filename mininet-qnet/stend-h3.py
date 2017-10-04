6ce7b18d22331e30ddbbba5525bea5a2adea9e84
Kompaniets Mikhail <mkompan@gmail.com>
2017-10-04 13:46:40 +0300

    modified confs

--- mininet-qnet/stend-h3.py
+++ mininet-qnet/stend-h3.py
@@ -2,5 +2,6 @@ config = { 'hostn': 3,
            'hosts': [5,],
            'intf' : 'eno1',
            'controller_ip' : None,
-           'controller_port': 6634
+           'controller_port': 6634,
+           'conf': 'conf'
          }
