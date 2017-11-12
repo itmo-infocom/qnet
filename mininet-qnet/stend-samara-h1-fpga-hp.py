0a77b904fa0c4ff9104413c60b7319ff38f9dd08
Mikhail Kompaniets <mkompan@gmail.com>
2017-11-13 02:15:30 +0400

    config drafts for smara

---
+++ mininet-qnet/stend-samara-h1-fpga-hp.py
@@ -0,0 +1,9 @@
+config = {
+           'controllers': {'c0': ('192.168.0.2',6633),},
+           'switches' : {'s1':'c0',},
+           'hosts': ['h1','h2'],
+           'phys': ['eno3',],
+           'links': [('s1','h1'),('s1','h2'),('s1','eno3'),],
+           'conf': 'conf2-fpga',
+           'rootns': {'s1':'10.0.0.100/8',}
+         }
---
+++ mininet-qnet/stend-samara-h3-hp.py
@@ -0,0 +1,9 @@
+config = {
+           'controllers': {'c0': (None,6634),},
+           'switches' : {'s3':'c0',},
+           'hosts': ['h5',],
+           'phys': ['eno3',],
+           'links': [('s3','h5'),('s3','eno3'),],
+           'conf': 'conf2',
+           'rootns': {'s3':'10.0.0.102/8',}
+         }
