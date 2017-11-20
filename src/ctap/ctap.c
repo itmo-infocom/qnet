de7d6458c669a1b26f3c808011a60d57f6e131ea
Petr <giggsoff@gmail.com>
2017-11-20 18:14:23 +0300

    auto run device

--- src/ctap/ctap.c
+++ src/ctap/ctap.c
@@ -41,10 +41,12 @@
 #define CLIENT 0 
 #define SERVER 1 
 #define PORT 55555
-int debug; char *progname; /**************************************************************************
+int debug; char *progname; 
+/**************************************************************************
  * tun_alloc: allocates or reconnects to a tun/tap device. The caller *
  * must reserve enough space in *dev.  *
- **************************************************************************/ int tun_alloc(char *dev, int flags) {
+ **************************************************************************/ 
+int tun_alloc(char *dev, int flags) {
   struct ifreq ifr;
   int fd, err;
   char *clonedev = "/dev/net/tun";
@@ -62,6 +64,11 @@ int debug; char *progname; /****************************************************
     close(fd);
     return err;
   }
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  ifr.ifr_flags |= IFF_UP | IFF_RUNNING;
+  if((err = ioctl(sock, SIOCSIFFLAGS, (void *)&ifr))<0){
+     perror("ioctl(FLAGS)");
+  }
   strcpy(dev, ifr.ifr_name);
   return fd;
 }
@@ -72,59 +79,11 @@ void print_hex(BYTE str[], int len)
 	for(idx = 0; idx < len; idx++)
 		printf("%02x", str[idx]);
 }
-int aes_cbc_test()
-{
-	WORD key_schedule[60];
-	BYTE enc_buf[128];
-	BYTE plaintext[1][32] = {
-		{0x6b,0xc1,0xbe,0xe2,0x2e,0x40,0x9f,0x96,0xe9,0x3d,0x7e,0x11,0x73,0x93,0x17,0x2a,0xae,0x2d,0x8a,0x57,0x1e,0x03,0xac,0x9c,0x9e,0xb7,0x6f,0xac,0x45,0xaf,0x8e,0x51}
-	};
-	BYTE ciphertext[1][32] = {
-		{0xf5,0x8c,0x4c,0x04,0xd6,0xe5,0xf1,0xba,0x77,0x9e,0xab,0xfb,0x5f,0x7b,0xfb,0xd6,0x9c,0xfc,0x4e,0x96,0x7e,0xdb,0x80,0x8d,0x67,0x9f,0x77,0x7b,0xc6,0x70,0x2c,0x7d}
-	};
-	BYTE iv[1][16] = {
-		{0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f}
-	};
-	BYTE key[1][32] = {
-		{0x60,0x3d,0xeb,0x10,0x15,0xca,0x71,0xbe,0x2b,0x73,0xae,0xf0,0x85,0x7d,0x77,0x81,0x1f,0x35,0x2c,0x07,0x3b,0x61,0x08,0xd7,0x2d,0x98,0x10,0xa3,0x09,0x14,0xdf,0xf4}
-	};
-	int pass = 1;
-
-	printf("* CBC mode:\n");
-	aes_key_setup(key[0], key_schedule, 256);
-
-	printf(  "Key          : ");
-	print_hex(key[0], 32);
-	printf("\nIV           : ");
-	print_hex(iv[0], 16);
-
-	aes_encrypt_cbc(plaintext[0], 32, enc_buf, key_schedule, 256, iv[0]);
-	printf("\nPlaintext    : ");
-	print_hex(plaintext[0], 32);
-	printf("\n-encrypted to: ");
-	print_hex(enc_buf, 32);
-	printf("\nCiphertext   : ");
-	print_hex(ciphertext[0], 32);
-	pass = pass && !memcmp(enc_buf, ciphertext[0], 32);
-
-	aes_decrypt_cbc(ciphertext[0], 32, enc_buf, key_schedule, 256, iv[0]);
-	printf("\nCiphertext   : ");
-	print_hex(ciphertext[0], 32);
-	printf("\n-decrypted to: ");
-	print_hex(enc_buf, 32);
-	printf("\nPlaintext   : ");
-	print_hex(plaintext[0], 32);
-	pass = pass && !memcmp(enc_buf, plaintext[0], 32);
-
-	printf("\n\n");
-	return(pass);
-}
-
 /**************************************************************************
  * cread: read routine that checks for errors and exits if an error is *
  * returned.  *
- **************************************************************************/ int cread(int fd, char *buf, int n){
-  
+ **************************************************************************/ 
+int cread(int fd, char *buf, int n){
   int nread;
   if((nread=read(fd, buf, n)) < 0){
     perror("Reading data");
@@ -135,8 +94,8 @@ int aes_cbc_test()
 /**************************************************************************
  * cwrite: write routine that checks for errors and exits if an error is *
  * returned.  *
- **************************************************************************/ int cwrite(int fd, char *buf, int n){
-  
+ **************************************************************************/ 
+int cwrite(int fd, char *buf, int n){
   int nwrite;
   if((nwrite=write(fd, buf, n)) < 0){
     perror("Writing data");
@@ -147,7 +106,8 @@ int aes_cbc_test()
 /**************************************************************************
  * read_n: ensures we read exactly n bytes, and puts them into "buf".  *
  * (unless EOF, of course) *
- **************************************************************************/ int read_n(int fd, char *buf, int n) {
+ **************************************************************************/ 
+int read_n(int fd, char *buf, int n) {
   int nread, left = n;
   while(left > 0) {
     if ((nread = cread(fd, buf, left)) == 0){
@@ -161,10 +121,9 @@ int aes_cbc_test()
 }
 /**************************************************************************
  * do_debug: prints debugging stuff (doh!)  *
- **************************************************************************/ void do_debug(char *msg, ...){
-  
+ **************************************************************************/ 
+void do_debug(char *msg, ...){
   va_list argp;
-  
   if(debug) {
 	va_start(argp, msg);
 	vfprintf(stderr, msg, argp);
@@ -173,16 +132,17 @@ int aes_cbc_test()
 }
 /**************************************************************************
  * my_err: prints custom error messages on stderr.  *
- **************************************************************************/ void my_err(char *msg, ...) {
+ **************************************************************************/ 
+void my_err(char *msg, ...) {
   va_list argp;
-  
   va_start(argp, msg);
   vfprintf(stderr, msg, argp);
   va_end(argp);
 }
 /**************************************************************************
  * usage: prints usage and exits.  *
- **************************************************************************/ void usage(void) {
+ **************************************************************************/ 
+void usage(void) {
   fprintf(stderr, "Usage:\n");
   fprintf(stderr, "%s -i <ifacename> [-s|-c <serverIP>] [-p <port>] [-u|-a] [-d]\n", progname);
   fprintf(stderr, "%s -h\n", progname);
@@ -195,8 +155,8 @@ int aes_cbc_test()
   fprintf(stderr, "-h: prints this help text\n");
   exit(1);
 }
+
 int main(int argc, char *argv[]) {
-  
   int tap_fd, option;
   int flags = IFF_TUN;
   char if_name[IFNAMSIZ] = "";
@@ -211,23 +171,22 @@ int main(int argc, char *argv[]) {
   int cliserv = -1; /* must be specified on cmd line */
   unsigned long int tap2net = 0, net2tap = 0;
   progname = argv[0];
-  	WORD key_schedule[60];
-	BYTE enc_buf[128];
-	BYTE plaintext[1][32] = {
+  WORD key_schedule[60];
+  BYTE enc_buf[128];
+  BYTE plaintext[1][32] = {
 		{0x6b,0xc1,0xbe,0xe2,0x2e,0x40,0x9f,0x96,0xe9,0x3d,0x7e,0x11,0x73,0x93,0x17,0x2a,0xae,0x2d,0x8a,0x57,0x1e,0x03,0xac,0x9c,0x9e,0xb7,0x6f,0xac,0x45,0xaf,0x8e,0x51}
-	};
-	BYTE ciphertext[1][32] = {
+  };
+  BYTE ciphertext[1][32] = {
 		{0xf5,0x8c,0x4c,0x04,0xd6,0xe5,0xf1,0xba,0x77,0x9e,0xab,0xfb,0x5f,0x7b,0xfb,0xd6,0x9c,0xfc,0x4e,0x96,0x7e,0xdb,0x80,0x8d,0x67,0x9f,0x77,0x7b,0xc6,0x70,0x2c,0x7d}
-	};
-	BYTE iv[1][16] = {
+  };
+  BYTE iv[1][16] = {
 		{0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f}
-	};
-	BYTE key[1][32] = {
+  };
+  BYTE key[1][32] = {
 		{0x60,0x3d,0xeb,0x10,0x15,0xca,0x71,0xbe,0x2b,0x73,0xae,0xf0,0x85,0x7d,0x77,0x81,0x1f,0x35,0x2c,0x07,0x3b,0x61,0x08,0xd7,0x2d,0x98,0x10,0xa3,0x09,0x14,0xdf,0xf4}
-	};
-	int pass = 1;
+  };
+  int pass = 1;
 
-  //aes_cbc_test();
   /* Check command line options */
   while((option = getopt(argc, argv, "i:sc:p:uahd")) > 0) {
     switch(option) {
@@ -301,7 +260,6 @@ int main(int argc, char *argv[]) {
     }
     net_fd = sock_fd;
     do_debug("CLIENT: Connected to server %s\n", inet_ntoa(remote.sin_addr));
-    
   } else {
     /* Server, wait for connections */
     /* avoid EADDRINUSE error on bind() */
